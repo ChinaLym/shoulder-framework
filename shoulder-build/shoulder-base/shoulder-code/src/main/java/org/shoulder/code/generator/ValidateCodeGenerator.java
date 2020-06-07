7d005c14f7967ab6e44869b97b21e2194efd07d0
15858193327 <lymdhr@qq.com>
2020-06-07 22:39:43 +0800

    add doc

--- shoulder-build/shoulder-base/shoulder-code/src/main/java/org/shoulder/code/generator/ValidateCodeGenerator.java
+++ shoulder-build/shoulder-base/shoulder-code/src/main/java/org/shoulder/code/generator/ValidateCodeGenerator.java
@@ -10,6 +10,11 @@ import org.springframework.web.context.request.ServletWebRequest;
  */
 public interface ValidateCodeGenerator extends ValidateCodeType {
 
+	/**
+	 * 生成验证码
+	 * @param request 请求，可以从这里获取参数
+	 * @return 验证码 DTO
+	 */
 	ValidateCodeDTO generate(ServletWebRequest request);
 	
 }
