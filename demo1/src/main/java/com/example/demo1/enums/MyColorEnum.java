2ad82f4605ff8bf021ed7ed947be7541f0c788cd
lym <lymdhr@qq.com>
2020-07-31 23:37:47 +0800

    delete useless

--- demo1/src/main/java/com/example/demo1/enums/MyColorEnum.java
+++ demo1/src/main/java/com/example/demo1/enums/MyColorEnum.java
@@ -1,7 +1,5 @@
 package com.example.demo1.enums;
 
-import java.io.Serializable;
-
 /**
  * 自定义枚举
  */
@@ -16,34 +14,4 @@ public enum MyColorEnum {
 
     ;
 
-}
-
-
-class SingleTon implements Serializable {
-    private static volatile SingleTon singleTon;
-
-    public static SingleTon getInstance(){
-        if(singleTon != null){
-            return singleTon;
-        }
-        synchronized (SingleTon.class) {
-            if(singleTon == null){
-                singleTon = new SingleTon();
-                return singleTon;
-            }
-        }
-    }
-
-    private SingleTon(){
-
-    }
-
-    @Override
-    public Object clone(){
-        return getInstance();
-    }
-
-    private Object readResolve(){
-        return getInstance();
-    }
 }
