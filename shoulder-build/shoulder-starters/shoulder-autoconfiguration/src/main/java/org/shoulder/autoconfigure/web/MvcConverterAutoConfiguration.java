8202923e6844c8b4e1a08ece0fdd04cb6d078460
lym <cn_lym@foxmail.com>
2020-07-31 19:13:01 +0800

    feat: 支持自动入参字符串转换任意枚举参数

--- shoulder-build/shoulder-starters/shoulder-autoconfiguration/src/main/java/org/shoulder/autoconfigure/web/MvcConverterAutoConfiguration.java
+++ shoulder-build/shoulder-starters/shoulder-autoconfiguration/src/main/java/org/shoulder/autoconfigure/web/MvcConverterAutoConfiguration.java
@@ -17,46 +17,60 @@ public class MvcConverterAutoConfiguration {
 
     /**
      * 将 String 类型入参，转为 Date 类型
+     *
      * @return DateConverter
      */
     @Bean
-    public DateConverter dateConverter(){
+    public DateConverter dateConverter() {
         return new DateConverter();
     }
 
     /**
      * 将 String 类型入参，转为 LocalDate 类型
+     *
      * @return LocalDateConverter
      */
     @Bean
-    public LocalDateConverter localDateConverter(){
+    public LocalDateConverter localDateConverter() {
         return new LocalDateConverter();
     }
 
     /**
      * 将 String 类型入参，转为 LocalDate 类型
+     *
      * @return LocalDateTimeConverter
      */
     @Bean
-    public LocalDateTimeConverter localDateTimeConverter(){
+    public LocalDateTimeConverter localDateTimeConverter() {
         return new LocalDateTimeConverter();
     }
 
     /**
      * 将 String 类型入参，转为 LocalTime 类型
+     *
      * @return LocalTimeConverter
      */
     @Bean
-    public LocalTimeConverter localTimeConverter(){
+    public LocalTimeConverter localTimeConverter() {
         return new LocalTimeConverter();
     }
 
     /**
+     * 字符串参数自动转枚举
+     * @return EnumConverterFactory
+     */
+    @Bean
+    public EnumConverterFactory enumConverterFactory() {
+        return new EnumConverterFactory();
+    }
+
+    /**
      * 默认的枚举转换失败处理器，若转换失败则返回 null
+     *
      * @return EnumMissMatchHandler
      */
     @Bean
-    public EnumMissMatchHandler enumMissMatchHandler(){
+    public EnumMissMatchHandler enumMissMatchHandler() {
         return new DefaultEnumMissMatchHandler();
     }
 
