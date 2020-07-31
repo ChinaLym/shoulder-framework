2a9deb0be0df8b36d4e6f6089aef1c2f5eebe8f4
lym <cn_lym@foxmail.cn>
2020-07-31 21:28:42 +0800

    update demo

--- demo1/src/main/java/com/example/demo1/enums/MyColorEnum.java
+++ demo1/src/main/java/com/example/demo1/enums/MyColorEnum.java
@@ -1,5 +1,7 @@
 package com.example.demo1.enums;
 
+import java.io.Serializable;
+
 /**
  * 自定义枚举
  */
@@ -14,4 +16,34 @@ public enum MyColorEnum {
 
     ;
 
+}
+
+
+class SingleTon implements Serializable {
+    private static volatile SingleTon singleTon;
+
+    public static SingleTon getInstance(){
+        if(singleTon != null){
+            return singleTon;
+        }
+        synchronized (SingleTon.class) {
+            if(singleTon == null){
+                singleTon = new SingleTon();
+                return singleTon;
+            }
+        }
+    }
+
+    private SingleTon(){
+
+    }
+
+    @Override
+    public Object clone(){
+        return getInstance();
+    }
+
+    private Object readResolve(){
+        return getInstance();
+    }
 }
