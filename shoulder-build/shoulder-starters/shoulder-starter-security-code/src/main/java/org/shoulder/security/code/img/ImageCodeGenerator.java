d4e33ec86180e8a78fef0ee57c2c7544e47ed9d6
15858193327 <lymdhr@qq.com>
2020-09-14 01:28:17 +0800

    perf(code) 提升验证码生成中随机数算法的性能，且避免线程竞争

--- shoulder-build/shoulder-starters/shoulder-starter-security-code/src/main/java/org/shoulder/security/code/img/ImageCodeGenerator.java
+++ shoulder-build/shoulder-starters/shoulder-starter-security-code/src/main/java/org/shoulder/security/code/img/ImageCodeGenerator.java
@@ -7,13 +7,12 @@ import org.springframework.web.context.request.ServletWebRequest;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.security.SecureRandom;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 默认的图片验证码生成器
  * 数字与字母
- * todo ThreadLocalRandom
  *
  * @author lym
  */
@@ -29,7 +28,6 @@ public class ImageCodeGenerator implements ValidateCodeGenerator, ImageValidateC
      * 系统配置
      */
     private ImageCodeProperties imageCodeProperties;
-    private Random random = new SecureRandom();
 
     public ImageCodeGenerator(ImageCodeProperties imageCodeProperties) {
         FONT_NAMES = imageCodeProperties.getFonts().toArray(new String[0]);
@@ -41,6 +39,7 @@ public class ImageCodeGenerator implements ValidateCodeGenerator, ImageValidateC
      */
     @Override
     public ImageCode generate(ServletWebRequest request) {
+        Random random = ThreadLocalRandom.current();
         // 1. 确定参数（图片宽、高，验证码长度）
         int width = ServletRequestUtils.getIntParameter(request.getRequest(), "width",
             imageCodeProperties.getWidth());
@@ -60,10 +59,12 @@ public class ImageCodeGenerator implements ValidateCodeGenerator, ImageValidateC
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics graphics = image.getGraphics();
 
-        // 画背景
-        graphics.setColor(nextRandomColor(200, 250));
-        graphics.fillRoundRect(0, 0, width, height, 10, 10);
-        // 干扰线
+        // 画背景，使用浅颜色
+        graphics.setColor(nextRandomColor(210, 250));
+        // 前4个参数：范围；后两个参数：圆角大小
+        graphics.fillRoundRect(0, 0, width, height, 0, 0);
+
+        // 干扰线，生成随机背景条纹
         graphics.setColor(nextRandomColor(110, 180));
         graphics.setFont(new Font("Times New Roman", Font.ITALIC, height - 10));
         int interferenceLineNum = 128;
@@ -75,8 +76,8 @@ public class ImageCodeGenerator implements ValidateCodeGenerator, ImageValidateC
             graphics.drawLine(x, y, x + xl, y + yl);
         }
 
-        // 画验证码
-        graphics.setColor(nextRandomColor(40, 170));
+        // 画验证码，使用深颜色
+        graphics.setColor(nextRandomColor(30, 140));
         for (int i = 0; i < codeLength; i++) {
             final int offset = 5;
             int fontSize = height - 20 + random.nextInt(10);
@@ -91,13 +92,13 @@ public class ImageCodeGenerator implements ValidateCodeGenerator, ImageValidateC
     }
 
     /**
-     * 生成随机背景条纹
      * 获取随机颜色，亮度越大越接近白色，越低越接近黑色
      *
      * @param minLight 颜色的最低亮度
      * @param maxLight 颜色的最大亮度
      */
     private Color nextRandomColor(int minLight, int maxLight) {
+        Random random = ThreadLocalRandom.current();
         int rbgMax = 255;
         if (minLight < 0 || minLight > maxLight || maxLight > rbgMax) {
             throw new IllegalArgumentException("no such color(min=" + minLight + ",max=" + maxLight + ")!");
