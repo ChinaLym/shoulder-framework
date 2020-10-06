d768e5e06f129283c70911b85d264eefcd96d096
15858193327 <lymdhr@qq.com>
2020-10-06 23:44:48 +0800

    feat(beanmap) add beanmap

---
+++ shoulder-build/shoulder-base/shoulder-web/src/main/java/org/shoulder/web/annotation/RestfulApi.java
@@ -0,0 +1,25 @@
+package org.shoulder.web.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.*;
+
+/**
+ * 统一返回值包装
+ * 在 RestController 基础上，该类所有方法跳过标准返回值包装
+ * 加方法上，返回值统一包装
+ * code、msg、data，其中接口返回值作为 data
+ *
+ * @author lym
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RestController
+public @interface RestfulApi {
+    @AliasFor(
+        annotation = RestController.class
+    )
+    String value() default "";
+}
