87639e845963ad035b0c36684b4f2fb423f13838
15858193327 <lymdhr@qq.com>
2020-10-29 00:30:46 +0800

    perf(guid): 压缩对象结构，解决缓存伪共享，充分利用缓存行并消除伪共享。 8线程生成1亿个所需时间 420-460ms -> 390-420ms.

--- shoulder-build/shoulder-base/shoulder-core/src/main/java/org/shoulder/core/uuid/InstanceIdProvider.java
+++ shoulder-build/shoulder-base/shoulder-core/src/main/java/org/shoulder/core/uuid/InstanceIdProvider.java
@@ -4,6 +4,7 @@ package org.shoulder.core.uuid;
 /**
  * 获取实例标识接口，用于 guid 中填充实例标识
  * 实现该接口即可修改获取方式，如固定、从配置文件中读取、从 redis、zookeeper、etcd 等
+ * 如果设计 instanceId 回放策略，一般是从队列头部取，在尾部放入
  *
  * @author lym
  */
