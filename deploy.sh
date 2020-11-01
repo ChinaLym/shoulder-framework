653b6cb956ece4bdfa6aa4072aa61d65deed6424
15858193327 <lymdhr@qq.com>
2020-11-02 02:01:06 +0800

    version: 0.4

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 mvn clean deploy -p oss -Dmaven.test.skip=true
 
 # 不要包含 -U，将强制下载最新 SNAPSHOT
-# 多线程并行编译-Dmaven.compile.fork=true，-T 指定每核线程数如，-Dmaven.compile.fork=true -T 2C
+# 多线程并行编译-Dmaven.compile.fork=true，-T 指定每核线程数如，-Dmaven.compile.fork=true -T 4C，IDEA 中直接输入线程数即可
 # 适当增加内存 -Xms1g -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256M
 # -Dmaven.test.skip=true 跳过测试
+# 发布前修改版本号
+# <shoulder.version>0.4</shoulder.version><!-- shoulder-version -->
+# <shoulder.version>0.4</shoulder.version><!-- shoulder-version -->
+# 开启部署前签名
+# 修改 <phase>verify</phase><!-- gpg phase verify deploy --> 至少提前至 verify 才可以保证发布前签名已经存在
+
+# ------------------
+# 新版本时，修改版本号
+# 修改签名阶段
--- shoulder-archetype-simple/JPA.POJO.groovy
+++ shoulder-archetype-simple/JPA.POJO.groovy
@@ -10,7 +10,7 @@ import com.intellij.database.util.DasUtil
  *   FILES       files helper
  */
 
-packageName = "me.test.entity;"
+packageName = "cn.itlym.shoulder;"
 typeMapping = [
     (~/(?i)bigint/)                   : "Long",
     (~/(?i)tinyint/)                  : "Boolean",
