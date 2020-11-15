23ecfacec7cc2f8c6f1360bd66da44a6db4c3739
15858193327 <lymdhr@qq.com>
2020-11-15 23:42:12 +0800

    feat(core-lock): 增加 lock 定义

--- shoulder-build/shoulder-base/shoulder-core/src/main/java/org/shoulder/core/lock/ReentrantServerLock.java
+++ shoulder-build/shoulder-base/shoulder-core/src/main/java/org/shoulder/core/lock/ReentrantServerLock.java
@@ -1,14 +1,13 @@
 package org.shoulder.core.lock;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 通过装饰者模式实现重入次数计数
- *
+ * <p>
  * 因为每次重入时，只需判断是否持有锁即可，无需更新中间件中的值，减少网络通信与写开销
  *
  * @author lym
@@ -21,33 +20,56 @@ public class ReentrantServerLock implements ServerLock {
      * 当前进程持有的锁的重入计数器
      * 锁标识 - 重入次数
      */
-    private ConcurrentMap<String, Integer> reentrantCount = new ConcurrentHashMap<>();
+    private ConcurrentMap<String, AtomicInteger> reentrantCountMap = new ConcurrentHashMap<>();
 
-    @Override
-    public boolean tryLock(Duration lockLife) {
-        /*if(delegate.tryLock()){
-
-        }*/
-        return false;
+    public ReentrantServerLock(ServerLock delegate) {
+        this.delegate = delegate;
     }
 
     @Override
-    public boolean tryLock(Duration maxWait, Duration lockLife) throws InterruptedException {
-        return false;
+    public boolean tryLock(LockInfo lockInfo, Duration maxBlockTime) throws InterruptedException {
+        if (delegate.holdLock(lockInfo)) {
+            reentrantCountMap.get(lockInfo.getResource()).incrementAndGet();
+            return true;
+        } else {
+            boolean lock = delegate.tryLock(lockInfo, maxBlockTime);
+            if (lock) {
+                reentrantCountMap.put(lockInfo.getResource(), new AtomicInteger(0));
+            }
+            return lock;
+        }
     }
 
     @Override
-    public boolean tryLock(String toLockResource, String valueEx, Duration holdDuration) {
-        return false;
+    public boolean tryLock(LockInfo lockInfo) {
+        if (delegate.holdLock(lockInfo)) {
+            reentrantCountMap.get(lockInfo.getResource()).incrementAndGet();
+            return true;
+        } else {
+            boolean lock = delegate.tryLock(lockInfo);
+            if (lock) {
+                reentrantCountMap.put(lockInfo.getResource(), new AtomicInteger(0));
+            }
+            return lock;
+        }
     }
 
     @Override
-    public boolean holdLock(String toLockResource, String valueEx) {
-        return false;
+    public LockInfo getLockInfo(String resource) {
+        return delegate.getLockInfo(resource);
     }
 
     @Override
-    public void release(String toLockResource, String valueEx) {
+    public boolean holdLock(String resource, String token) {
+        return delegate.holdLock(resource, token);
+    }
 
+    @Override
+    public void unlock(String resource, String token) {
+        if (holdLock(resource, token)) {
+            if (reentrantCountMap.get(resource).decrementAndGet() == 0) {
+                delegate.unlock(resource, token);
+            }
+        }
     }
 }
---
+++ shoulder-build/shoulder-base/shoulder-core/src/test/java/org/shoulder/core/lock/LockTest.java
@@ -0,0 +1,54 @@
+package org.shoulder.core.lock;
+
+import org.junit.Test;
+import org.shoulder.core.lock.impl.MemoryLockAbstract;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class LockTest {
+
+    /**
+     * 测试内存锁
+     */
+    @Test
+    public void testMemoryLock() throws InterruptedException {
+        testExclusion(new MemoryLockAbstract());
+    }
+
+
+    /**
+     * 互斥性测试
+     *
+     * @param lock lock
+     */
+    public void testExclusion(ServerLock lock) throws InterruptedException {
+        int sleepSecond = 1;
+        Set<Integer> mightExecutionTime = ConcurrentHashMap.newKeySet();
+        mightExecutionTime.add(sleepSecond);
+        mightExecutionTime.add(sleepSecond << 1);
+        Runnable testLockRunnable = () -> {
+            LockInfo lockInfo = new LockInfo("test");
+            Instant preLock = Instant.now();
+            lock.lock(lockInfo);
+            // 断言获取到了
+            assert lock.holdLock(lockInfo.getResource(), lockInfo.getToken());
+            try {
+                Thread.sleep(sleepSecond * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lock.unlock(lockInfo);
+            Instant afterLock = Instant.now();
+            long seconds = Duration.between(preLock, afterLock).toSeconds();
+            assert mightExecutionTime.remove((int) seconds);
+        };
+        new Thread(testLockRunnable).start();
+        new Thread(testLockRunnable).start();
+        Thread.sleep(sleepSecond * 1000 + 1000);
+        System.out.println("ok");
+    }
+
+}
