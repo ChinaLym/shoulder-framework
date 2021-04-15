f2dee0066fbe11ca4c62f92f82af1a19965ea937
15858193327 <lymdhr@qq.com>
2021-04-15 23:57:36 +0800

    feat: 优化 ThreadEnhancer：优化内存、拨离性能

--- shoulder-build/shoulder-base/shoulder-core/src/main/java/org/shoulder/core/concurrent/enhance/EnhancedRunnable.java
+++ shoulder-build/shoulder-base/shoulder-core/src/main/java/org/shoulder/core/concurrent/enhance/EnhancedRunnable.java
@@ -1,5 +1,7 @@
 package org.shoulder.core.concurrent.enhance;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,7 +13,7 @@ import java.util.List;
 public class EnhancedRunnable implements Runnable {
 
     /**
-     * 被装饰的原生 runnable，todo 约束不出意外，一定是 EnhancedRunnable
+     * 被装饰的原生 runnable
      */
     protected Runnable delegate;
 
@@ -20,25 +22,12 @@ public class EnhancedRunnable implements Runnable {
         delegate.run();
     }
 
-    /**
-     * 血统（装饰器的类）
-     */
-    private List<Runnable> decorators = new LinkedList<>();
-
-
     EnhancedRunnable() {
 
     }
 
     public EnhancedRunnable(Runnable delegate) {
         this.delegate = delegate;
-        if (delegate instanceof EnhancedRunnable) {
-            EnhancedRunnable enhancedRunnable = (EnhancedRunnable) delegate;
-            this.decorators.addAll(enhancedRunnable.getDecorators());
-            this.decorators.add(enhancedRunnable);
-        } else {
-            this.decorators.add(delegate);
-        }
     }
 
     /**
@@ -48,11 +37,13 @@ public class EnhancedRunnable implements Runnable {
      * @return 结果
      */
     public boolean isInstanceOf(Class<?> clazz) {
-        if (clazz.isAssignableFrom(getClass())) {
+        if (clazz.isInstance(this)) {
             return true;
         }
-        for (Runnable decorator : decorators) {
-            if (clazz.isAssignableFrom(decorator.getClass())) {
+        Runnable result = this;
+        while (result instanceof EnhancedRunnable) {
+            result = ((EnhancedRunnable) result).delegate;
+            if (clazz.isInstance(result)) {
                 return true;
             }
         }
@@ -61,11 +52,18 @@ public class EnhancedRunnable implements Runnable {
 
     /**
      * 所有包装过的装饰器
+     * origin 不再内
      *
      * @return List class
      */
     public List<Runnable> getDecorators() {
-        return decorators;
+        List<Runnable> runnableList = new LinkedList<>();
+        Runnable result = this;
+        while (result instanceof EnhancedRunnable) {
+            runnableList.add(result);
+            result = ((EnhancedRunnable) result).delegate;
+        }
+        return runnableList;
     }
 
     /**
@@ -74,45 +72,48 @@ public class EnhancedRunnable implements Runnable {
      * @return class
      */
     public Runnable getOrigin() {
-        return decorators.get(0);
+        Runnable result = this;
+        while (result instanceof EnhancedRunnable) {
+            result = ((EnhancedRunnable) result).delegate;
+        }
+        return result;
     }
 
     /**
-     * 添加血脉
-     */
-    public void addDecorator(Runnable ancestry) {
-        decorators.add(ancestry);
-    }
-
-    /**
-     * 把本 runnable 当作
-     * 寻找到最外层的包装器，返回
+     * 把本 runnable 当作 clazz
      *
      * @throws ClassCastException clazz 错误，未被目标类包装过
      */
+    @Nonnull
     public <T> T as(Class<T> clazz) {
-        if (clazz.isAssignableFrom(getClass())) {
-            return (T) this;
+        T result = unwrap(clazz);
+        if (result != null) {
+            return result;
         }
-        // 计算这个类被包装了多少次
-        int warpCount = 0;
-        for (int i = decorators.size() - 1; i >= 0; i--, warpCount++) {
-            if (clazz.isAssignableFrom(decorators.get(i).getClass())) {
-                break;
+        throw new ClassCastException("can't cast to " + clazz.getName());
+    }
+
+    /**
+     * 寻找到最外层的 clazz 包装器，返回
+     * <p>
+     * 与 lettuce.core 类似
+     *
+     * @return 找不到则返回 null
+     * @see io.lettuce.core.protocol.CommandWrapper#unwrap
+     */
+    @Nullable
+    public <T> T unwrap(Class<T> clazz) {
+        if (clazz.isInstance(this)) {
+            return clazz.cast(this);
+        }
+        Runnable result = this;
+        while (result instanceof EnhancedRunnable) {
+            result = ((EnhancedRunnable) result).delegate;
+            if (clazz.isInstance(result)) {
+                return clazz.cast(result);
             }
         }
-        if (warpCount > decorators.size()) {
-            // 一定没有被目标类增强过
-            throw new ClassCastException("can't cast to " + clazz.getName());
-        }
-        // 开始剥皮
-        Runnable target = this;
-        do {
-            // todo 这里认为一定是 EnhancedRunnable；
-            target = ((EnhancedRunnable) target).delegate;
-            warpCount--;
-        } while (warpCount >= 0);
-        return (T) target;
+        return null;
     }
 
 }
