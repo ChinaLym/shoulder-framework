4004cb4b43e24b53eb481c51a6fb9a83a76daecf
15858193327 <lymdhr@qq.com>
2021-05-15 17:52:04 +0800

    feat: ext-module

--- shoulder-build/shoulder-plugins/shoulder-ext-config/src/main/java/org/shoulder/ext/config/config/BackstageInterceptorConfig.java
+++ shoulder-build/shoulder-plugins/shoulder-ext-config/src/main/java/org/shoulder/ext/config/config/BackstageInterceptorConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
+/**
+ * @author lym
+ */
 @Configuration
 public class BackstageInterceptorConfig {
 
