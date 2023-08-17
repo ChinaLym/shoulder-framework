58329c15df01e679c138dada04322337406211a6
15858193327 <lymdhr@qq.com>
2023-08-18 00:27:56 +0800

    cicd fast

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-mvn clean deploy -p oss -Dmaven.test.skip=true
+mvn clean deploy -p oss -Dmaven.test.skip=true -T 1C
 
 # 不要包含 -U，将强制下载最新 SNAPSHOT
-# 多线程并行编译-Dmaven.compile.fork=true，-T 指定每核线程数如，-Dmaven.compile.fork=true -T 4C，IDEA 中直接输入线程数即可
-# 适当增加内存 -Xms1g -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256M
+# 多线程并行编译-Dmaven.compile.fork=true，-T 指定每核线程数如，-Dmaven.compile.fork=true -T 2C，IDEA 中直接输入线程数即可
+# 本工程较大，需要适当增加内存 -Xms1g -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256M
 # -Dmaven.test.skip=true 跳过测试
 
 
