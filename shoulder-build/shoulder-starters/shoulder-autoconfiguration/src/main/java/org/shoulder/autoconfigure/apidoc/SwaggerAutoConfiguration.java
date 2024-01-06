80bd12e11547f6c1fddf3545cefa1a8a1a256f7e
lym <lymdhr@qq.com>
2024-01-06 12:51:49 +0800

    1. jdk：11 -> 17 2. AppContext: Object must be Serializable to Serialize and copy, avoid SerializeException. 3. imporve doc/log 4. javax -> jakarta(部分命名空间未改变 https://www.agilejava.eu/2021/01/22/migration-guide/) 5. 暂时移除仍然使用 javax 的依赖： com.google.code.findbugs、springfox，直到其更新为 jakarta （同spring） 6. 适配 Spring Security 5.7+ 改动（移除WebSecurityConfigurerAdapter）暂时删除 TokenSecurityConfiguration，BrowserSecurityConfiguration能力（本就只是demo不建议直接使用） https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter/
    spring-boot:2.7.16 -> 3.1.x
    spring-cloud:2021.0.8 -> 2022.x

--- shoulder-build/shoulder-starters/shoulder-autoconfiguration/src/main/java/org/shoulder/autoconfigure/apidoc/SwaggerAutoConfiguration.java
+++ shoulder-build/shoulder-starters/shoulder-autoconfiguration/src/main/java/org/shoulder/autoconfigure/apidoc/SwaggerAutoConfiguration.java
@@ -1,132 +1,132 @@
-package org.shoulder.autoconfigure.apidoc;
-
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
-import jakarta.annotation.Nonnull;
-import org.shoulder.autoconfigure.apidoc.util.RequestHandlerSelectors;
-import org.shoulder.core.context.AppInfo;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
-import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.stream.Collectors;
-
-/**
- * 访问
- * 在浏览器输入地址：http://host:port/doc.html
- * 默认地址：http://127.0.0.1:8080/doc.html
- * <p>
- * ApiDoc 相关配置
- *
- * @author lym
- */
-@AutoConfiguration
-@EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
-@ConditionalOnClass(value = {Docket.class})
-@ConditionalOnProperty(value = "shoulder.apidoc.default.enable", havingValue = "true", matchIfMissing = true)
-public class SwaggerAutoConfiguration {
-
-    public SwaggerAutoConfiguration() {
-        // just for debug
-    }
-
-    @AutoConfiguration
-    @ConditionalOnClass(EnableKnife4j.class)
-    @EnableKnife4j
-    public static class EnableKnife4jEnhance {
-        // just for debug
-    }
-
-    /**
-     * swagger3的配置文件
-     */
-    @Bean
-    @ConditionalOnMissingBean(value = Docket.class, name = "defaultDocket")
-    public Docket defaultDocket(ApiInfo apiInfo) {
-        return new Docket(DocumentationType.OAS_30)
-            .apiInfo(apiInfo)
-            .groupName("default")
-            .select()
-            // 包路径中带有 controller
-            .apis(RequestHandlerSelectors.packageRegex("^.*?\\.controller\\..*?$"))
-            .paths(PathSelectors.any())
-            .build();
-    }
-
-    /**
-     * 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public ApiInfo apiInfo() {
-        // 获取工程名称
-        String projectName = AppInfo.appId();
-        return new ApiInfoBuilder()
-                .title(projectName + " API 接口文档")
-                .contact(new Contact("lym", "https://github.com/ChinaLym/shoulder-framework", "cn_lym@foxmail.com"))
-                .version(AppInfo.version())
-                .description("默认 api 分组")
-                .build();
-    }
-
-    /**
-     * Spring Boot 2.6 与 swagger 不兼容解决
-     */
-    @AutoConfiguration
-    @ConditionalOnClass(WebMvcRequestHandlerProvider.class)
-    public static class FixNpeForSpringfoxHandlerProviderBeanPostProcessorConfiguration {
-
-        @Bean
-        public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-            return new BeanPostProcessor() {
-
-                @Override
-                public Object postProcessAfterInitialization(@Nonnull Object bean, @Nonnull String beanName) throws BeansException {
-                    if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
-                        customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
-                    }
-                    return bean;
-                }
-
-                private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
-                    List<T> copy = mappings.stream()
-                            .filter(mapping -> mapping.getPatternParser() == null)
-                            .collect(Collectors.toList());
-                    mappings.clear();
-                    mappings.addAll(copy);
-                }
-
-                @SuppressWarnings("unchecked")
-                private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
-                    try {
-                        Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
-                        field.setAccessible(true);
-                        return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
-                    } catch (IllegalArgumentException | IllegalAccessException e) {
-                        throw new IllegalStateException(e);
-                    }
-                }
-            };
-        }
-    }
-
-}
+//package org.shoulder.autoconfigure.apidoc;
+//
+//import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+//import jakarta.annotation.Nonnull;
+//import org.shoulder.autoconfigure.apidoc.util.RequestHandlerSelectors;
+//import org.shoulder.core.context.AppInfo;
+//import org.springframework.beans.BeansException;
+//import org.springframework.beans.factory.config.BeanPostProcessor;
+//import org.springframework.boot.autoconfigure.AutoConfiguration;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.util.ReflectionUtils;
+//import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+//import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+//import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//import java.lang.reflect.Field;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+///**
+// * 访问
+// * 在浏览器输入地址：http://host:port/doc.html
+// * 默认地址：http://127.0.0.1:8080/doc.html
+// * <p>
+// * ApiDoc 相关配置
+// *
+// * @author lym
+// */
+//@AutoConfiguration temp_removal_for_not_support_spring_boot_3
+//@EnableSwagger2
+//@Import(BeanValidatorPluginsConfiguration.class)
+//@ConditionalOnClass(value = {Docket.class})
+//@ConditionalOnProperty(value = "shoulder.apidoc.default.enable", havingValue = "true", matchIfMissing = true)
+//public class SwaggerAutoConfiguration {
+//
+//    public SwaggerAutoConfiguration() {
+//        // just for debug
+//    }
+//
+//    @AutoConfiguration
+//    @ConditionalOnClass(EnableKnife4j.class)
+//    @EnableKnife4j
+//    public static class EnableKnife4jEnhance {
+//        // just for debug
+//    }
+//
+//    /**
+//     * swagger3的配置文件
+//     */
+//    @Bean
+//    @ConditionalOnMissingBean(value = Docket.class, name = "defaultDocket")
+//    public Docket defaultDocket(ApiInfo apiInfo) {
+//        return new Docket(DocumentationType.OAS_30)
+//            .apiInfo(apiInfo)
+//            .groupName("default")
+//            .select()
+//            // 包路径中带有 controller
+//            .apis(RequestHandlerSelectors.packageRegex("^.*?\\.controller\\..*?$"))
+//            .paths(PathSelectors.any())
+//            .build();
+//    }
+//
+//    /**
+//     * 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+//     */
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public ApiInfo apiInfo() {
+//        // 获取工程名称
+//        String projectName = AppInfo.appId();
+//        return new ApiInfoBuilder()
+//                .title(projectName + " API 接口文档")
+//                .contact(new Contact("lym", "https://github.com/ChinaLym/shoulder-framework", "cn_lym@foxmail.com"))
+//                .version(AppInfo.version())
+//                .description("默认 api 分组")
+//                .build();
+//    }
+//
+//    /**
+//     * Spring Boot 2.6 与 swagger 不兼容解决
+//     */
+//    @AutoConfiguration
+//    @ConditionalOnClass(WebMvcRequestHandlerProvider.class)
+//    public static class FixNpeForSpringfoxHandlerProviderBeanPostProcessorConfiguration {
+//
+//        @Bean
+//        public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+//            return new BeanPostProcessor() {
+//
+//                @Override
+//                public Object postProcessAfterInitialization(@Nonnull Object bean, @Nonnull String beanName) throws BeansException {
+//                    if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+//                        customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+//                    }
+//                    return bean;
+//                }
+//
+//                private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+//                    List<T> copy = mappings.stream()
+//                            .filter(mapping -> mapping.getPatternParser() == null)
+//                            .collect(Collectors.toList());
+//                    mappings.clear();
+//                    mappings.addAll(copy);
+//                }
+//
+//                @SuppressWarnings("unchecked")
+//                private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+//                    try {
+//                        Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+//                        field.setAccessible(true);
+//                        return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+//                    } catch (IllegalArgumentException | IllegalAccessException e) {
+//                        throw new IllegalStateException(e);
+//                    }
+//                }
+//            };
+//        }
+//    }
+//
+//}
