488512d78b49b7f0d5bb47ab89a8567b35422512
lym <lymdhr@qq.com>
2024-04-21 01:48:31 +0800

    test gogs cicd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 # install script ==========
 export MAVEN_OPTS="-Xmx400m -Xms400m"
+mvn clean install -P release -Dmaven.test.skip=true -B -V -T 1.5C
 echo 'FINISHED TO [install] !!'
 # install script end ==========
