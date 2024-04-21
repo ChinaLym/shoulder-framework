875cc3e8ecaec69137e8d37e60870ad4fe814be3
gogs <cn_lym@foxmail.com>
2024-04-21 09:50:13 +0800

    更新 'scripts/prepareRelease.sh'

--- scripts/prepareRelease.sh
+++ scripts/prepareRelease.sh
@@ -1,7 +1,7 @@
 # remove -SNAPSHOT
 sed -i 's#-SNAPSHOT</shoulder.version>#</shoulder.version>#g' pom.xml
 sed -i 's#-SNAPSHOT</shoulder.version>#</shoulder.version>#g' shoulder-archetype-simple/src/main/resources/archetype-resources/pom.xml
-find . -name "pom.xml" | xargs sed -i 's#-SNAPSHOT</version><!-- shoulder-version -->#</shoulder.version>#g'
+find . -name "pom.xml" | xargs sed -i 's#-SNAPSHOT</version><!-- shoulder-version -->#</version><!-- shoulder-version -->#g'
 # add source and javadoc in shoulder-xxx.jar
 sed -i 's#<phase>deploy</phase><!-- ACTIVE_WITH_package -->#<phase>package</phase><!-- ACTIVE_WITH_package -->#g' pom.xml
 sed -i 's#<phase>deploy</phase><!-- ACTIVE_WITH_package -->#<phase>package</phase><!-- ACTIVE_WITH_package -->#g' shoulder-parent/pom.xml
