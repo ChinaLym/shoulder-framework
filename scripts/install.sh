cc99576e04ec54191c1ef55c41fe6251cef7eeb4
gogs <cn_lym@foxmail.com>
2024-04-21 10:58:53 +0800

    更新 'scripts/install.sh'

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,4 @@
 # install script ==========
 export MAVEN_OPTS="-Xmx400m -Xms400m"
-mvn clean install -P release -Dmaven.test.skip=true -B -V -T 1.5C
-echo 'FINISHED TO [install] !!'
+return mvn clean install -P release -Dmaven.test.skip=true -B -V -T 1.5C
 # install script end ==========
