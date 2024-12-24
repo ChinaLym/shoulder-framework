e2806d0164723aa67e4c4189baa9d0b51d14138e
lym <cnlym@foxmail.com>
2024-12-24 16:12:19 +0800

    feature 同时支持 json 与 str 转字典枚举

--- shoulder-build/shoulder-base/shoulder-web/src/main/java/org/shoulder/web/template/dictionary/convert/DictionaryItemDTODeserializer.java
+++ shoulder-build/shoulder-base/shoulder-web/src/main/java/org/shoulder/web/template/dictionary/convert/DictionaryItemDTODeserializer.java
@@ -3,11 +3,15 @@ package org.shoulder.web.template.dictionary.convert;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.std.UntypedObjectDeserializer;
 import org.shoulder.core.dictionary.model.DictionaryItemEnum;
+import org.shoulder.core.util.AssertUtils;
 import org.shoulder.core.util.ConvertUtil;
+import org.shoulder.validate.exception.ParamErrorCodeEnum;
 import org.shoulder.web.template.dictionary.dto.DictionaryItemDTO;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 支持前端传 String，后端用 DictionaryItemDTO 接收
@@ -28,15 +32,25 @@ public abstract class DictionaryItemDTODeserializer extends JsonDeserializer<Dic
     @Override
     public DictionaryItemDTO deserialize(JsonParser p, DeserializationContext context) throws IOException {
         String text = p.getText();
+
         if (text == null || text.isEmpty()) {
             return null;
         }
-//        if (!text.startsWith("{") && text.endsWith("}")) {
+
+        if (!text.startsWith("{")) {
+            Object parseResult = new UntypedObjectDeserializer().deserialize(p, context);
+            AssertUtils.isTrue(parseResult != null && parseResult instanceof Map, ParamErrorCodeEnum.PARAM_ILLEGAL, context.getParser().getParsingContext().getCurrentName());
+            Map parsedMap = (Map) parseResult;
+            AssertUtils.notEmpty(parsedMap, ParamErrorCodeEnum.PARAM_ILLEGAL, context.getParser().getParsingContext().getCurrentName());
+            String dictionaryCode = (String) parsedMap.get("code");
+            AssertUtils.notBlank(dictionaryCode, ParamErrorCodeEnum.PARAM_ILLEGAL, context.getParser().getParsingContext().getCurrentName());
+
+            DictionaryItemEnum dictionaryItemEnum = ConvertUtil.convert(dictionaryCode, dictionaryClass);
+            return ConvertUtil.convert(dictionaryItemEnum, DictionaryItemDTO.class);
+        }
+
         DictionaryItemEnum dictionaryItemEnum = ConvertUtil.convert(text, dictionaryClass);
         return ConvertUtil.convert(dictionaryItemEnum, DictionaryItemDTO.class);
-//        }
-        // todo 直接转化
-//        return null;
 
     }
 }
