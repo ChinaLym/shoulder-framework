a34e59647eedb4dbf3926a4ddb242a0dbf55c094
lym <lymdhr@qq.com>
2025-01-26 12:40:12 +0800

    add link

--- shoulder-archetype-simple/src/main/resources/archetype-resources/src/main/java/__StartClassName__.java
+++ shoulder-archetype-simple/src/main/resources/archetype-resources/src/main/java/__StartClassName__.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 启动类
- * <p><a href="https://doc.itlym.cn/shoulder.html">Shoulder 指南文档</a>
- * <p><a href="https://doc.itlym.cn/index.html#%E8%BD%AF%E4%BB%B6%E8%AE%BE%E8%AE%A1%E4%B8%8E%E5%BC%80%E5%8F%91%E8%A7%84%E8%8C%83">查看开发小技巧</a>
+ * <p><a href="https://doc.itlym.cn/shoulder.html?s=archetype">Shoulder 指南文档</a>
+ * <p><a href="https://doc.itlym.cn/index.html#%E8%BD%AF%E4%BB%B6%E8%AE%BE%E8%AE%A1%E4%B8%8E%E5%BC%80%E5%8F%91%E8%A7%84%E8%8C%83?s=archetype">查看开发小技巧</a>
  *
  * @author ${author}
  */
