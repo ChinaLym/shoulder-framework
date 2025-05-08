581b791fd368303da87610fdc6e46d091ef7961e
lym <lymdhr@qq.com>
2025-05-08 21:10:33 +0800

    prepare 1.1

--- scripts/prepareRelease_M1.sh
+++ scripts/prepareRelease_M1.sh
@@ -4,7 +4,7 @@ rm -rf /root/.m2/repository/cn/itlym
 sed -i 's#-SNAPSHOT</shoulder.version>#-M1</shoulder.version>#g' shoulder-dependencies/pom.xml
 sed -i 's#-SNAPSHOT</shoulder.version>#-M1</shoulder.version>#g' shoulder-archetype-simple/src/main/resources/archetype-resources/pom.xml
 find . -name "pom.xml" | xargs sed -i 's#-SNAPSHOT</version><!-- shoulder-version -->#-M1</version><!-- shoulder-version -->#g'
-find . -name "*.md" | xargs sed -i 's#DarchetypeVersion=0.8.1#DarchetypeVersion=1.0.0-PRE#g'
+find . -name "*.md" | xargs sed -i 's#DarchetypeVersion=0.8.1#DarchetypeVersion=1.1.0#g'
 # add source and javadoc in shoulder-xxx.jar
 sed -i 's#<phase>deploy</phase><!-- ACTIVE_WITH_package -->#<phase>package</phase><!-- ACTIVE_WITH_package -->#g' pom.xml
 sed -i 's#<phase>deploy</phase><!-- ACTIVE_WITH_package -->#<phase>package</phase><!-- ACTIVE_WITH_package -->#g' shoulder-parent/pom.xml
